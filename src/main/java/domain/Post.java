776abfc99ef5b00fa1cf951d6a3dc74278552779
PetrIvChe <cherniavskiipetr@gmail.com>
2021-10-20 14:02:29 +0300

    Added src/ files

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Post {
+    private String postUrl;
+    private String namePost;
+    private String idPost;
+    private String idAuthor;
+    private String commitUrl;
+    private String idCommit;
+    private String idAuthorOfCommit;
+
+    //getters/setters;
+}
