683ea4836b0c51f95e0d43a95ec3d5acc8c99a2d
PetrIvChe <cherniavskiipetr@gmail.com>
2021-10-20 15:02:21 +0300

    v2

--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -1,13 +1,31 @@
 package domain;
 
 public class Post {
-    private String postUrl;
-    private String namePost;
-    private String idPost;
-    private String idAuthor;
-    private String commitUrl;
-    private String idCommit;
-    private String idAuthorOfCommit;
+    private int id;
+    private int ownerId;
+    private  int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+    private int replyOwnerId;
+    private boolean friendOnly;
+    private CommentsInfo commentsInfo;
+    private Copyright copyright;
+    private LikesInfo likesinfo;
+    private ReportsInfo reportsInfo;
+    private ViewsInfo viewsInfo;
+    private  GeoInfo geoInfo;
+    private int signerId;
+    private boolean canPin;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean isPinned;
+    private boolean markedAsAds;
+    private boolean isFavorite;
+    private DonutInfo donutInfo;
+    private int postponedId
+
+
 
     //getters/setters;
 }
