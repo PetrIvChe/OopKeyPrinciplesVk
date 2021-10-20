8c00023ae79d43816f38b5b292f19067ecef6180
PetrIvChe <cherniavskiipetr@gmail.com>
2021-10-20 15:02:21 +0300

    v2

--- src/main/java/domain/CommentsInfo.java
+++ src/main/java/domain/CommentsInfo.java
@@ -3,6 +3,8 @@ package domain;
 public class CommentsInfo {
     private int count;
     private boolean canPost;
-
+    private boolean groupsCanPost;
+    private boolean canClose;
+    private boolean canOpen;
     // + get/set на все поля
 }
