df434c68d633e43289a2e98aec8a493bb1de3ca2
PetrIvChe <cherniavskiipetr@gmail.com>
2021-10-20 15:06:22 +0300

    v2

---
+++ src/main/java/domain/PlaceInfo.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class PlaceInfo {
+    private int id;
+    private String title;
+    private int latitude;
+    private int longitude;
+    private int created;
+    private String icon;
+    private int checkins;
+    private int updated;
+    private int type;
+    private int country;
+    private int City;
+    private String address;
+
+}
